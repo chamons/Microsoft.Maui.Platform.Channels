1d42317911cfc383e10ebd6ec5ee7aa23016f13a
Chris Hamons <chris.hamons@xamarin.com>
2022-07-21 14:24:06 -0500

    Finally

--- Platforms/Apple/Sample/Sample/MathChannel.swift
+++ Platforms/Apple/Sample/Sample/MathChannel.swift
@@ -11,6 +11,7 @@ import DotNetPlatformChannels
 class MathChannel : Channel {
     override func onChannelMessage (_ messageId: NSString, withArgs args: [NSObject]) -> NSObject? {
         let result = NSObject()
+        write_log ("onChannelMessage - math")
 
         if (messageId == "add") {
             if let a = args[0] as? NSNumber {
